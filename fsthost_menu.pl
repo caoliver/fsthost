173880a6389072cc6279588d02b77e69c4f3465f
xxj <xxj@75a15a69-0cfc-47d9-a1bd-ce2941b697b2>
2013-07-25 12:05:42 +0000

    menu - add more columns and label for display executed command
    git-svn-id: https://svn.code.sf.net/p/fsthost/code/trunk@246 75a15a69-0cfc-47d9-a1bd-ce2941b697b2

--- fsthost_menu.pl
+++ fsthost_menu.pl
@@ -8,25 +8,43 @@ use Gtk3;
 my $FSTHOST_GUI = 2; # 0 - no gui , 1 - hide, 2 - normal
 
 my $filename = $ENV{'HOME'}."/.fsthost.xml";
+my $label;
 
-sub start_fsthost { 
+sub get_cmd_from_tv {
 	my $tv = shift;
-	my ($model, $iter) = $tv->get_selection->get_selected();
+	my $selection = $tv->get_selection;
+	return "" if not $selection;
+
+	my ($model, $iter) = $selection->get_selected();
+
 	# Get reference to our part of %fst hash
 	my $arch = $model->get($iter, 1);
 	my $path = $model->get($iter, 2);
 
+	return "env FSTHOST_GUI=$FSTHOST_GUI fsthost$arch \"$path\" >/dev/null 2>&1 &";
+}
 
-	my $cmd = "env FSTHOST_GUI=$FSTHOST_GUI fsthost$arch \"$path\" >/dev/null 2>&1 &";
+sub start_fsthost { 
+	my $tv = shift;
 
+	my $cmd = get_cmd_from_tv($tv);
+	if ( not $cmd ) {
+		print "Empty cmd ?\n";
+		return;
+	}
 	print "Spawn: $cmd\n";
 	system ( $cmd );
+}
+
+sub tv_selection_changed {
+	my $tv = shift;
+
+	$label->set_text ( get_cmd_from_tv($tv) );
 }
 
 sub read_xml_db {
 	my $fst = shift;
 
-	# Parse XML DB
 	my $parser = XML::LibXML->new();
 	my $doc    = $parser->parse_file($filename);
 	my $root = $doc->documentElement();
@@ -51,6 +69,7 @@ read_xml_db ( \%fst );
 Gtk3->init; # works if you didn't use -init on use
 my $window = Gtk3::Window->new ('toplevel');
 $window->signal_connect('delete_event' => sub { Gtk3->main_quit; });
+#$window->set_icon_from_file ( 'fsthost.xpm', 0 );
 #$window->set_border_width(5);
  
 # VBox
@@ -66,56 +85,69 @@ $sw->set_policy ('automatic', 'automatic');
 #This is a method of the Gtk3::Widget class,it will force a minimum 
 #size on the widget. Handy to give intitial size to a 
 #Gtk3::ScrolledWindow class object
-$sw->set_size_request (300, 500);
+$sw->set_size_request (700, 700);
 #method of Gtk3::Container
 #$sw->set_border_width(5);
-$vbox->pack_start($sw, 1, 1,0);
+$vbox->pack_start($sw, 1, 1, 0);
 
 # TreeView
 my $tree_store = Gtk3::TreeStore->new(qw/Glib::String Glib::String Glib::String/);
 
 #fill it with arbitry data
-foreach (keys %fst) { 	
+foreach (sort keys %fst) { 	
 	#the iter is a pointer in the treestore. We use to add data.
 	my $iter = $tree_store->append(undef);
 	$tree_store->set ($iter, 0 => $_, 1 => $fst{$_}->{'arch'}, 2 => $fst{$_}->{'path'});
 }
+
+#create a renderer that will be used to display info in the model
+my $renderer = Gtk3::CellRendererText->new();
 
 #this will create a treeview, specify $tree_store as its model
 my $tree_view = Gtk3::TreeView->new($tree_store);
 
 # Connect double-click signal
 $tree_view->signal_connect('row-activated' => \&start_fsthost );
+$tree_view->signal_connect('cursor-changed' => \&tv_selection_changed );
 
-#create a Gtk3::TreeViewColumn to add to $tree_view
-my $tree_column = Gtk3::TreeViewColumn->new();
-$tree_column->set_title ("Plugins");
+my @columns_desc = ( 'Plugin', 'Arch' , 'Path' );
+foreach (0 .. 2) {
+	#create a Gtk3::TreeViewColumn to add to $tree_view
+	my $tree_column = Gtk3::TreeViewColumn->new();
+	$tree_column->set_title ( $columns_desc[$_] );
 
-#create a renderer that will be used to display info in the model
-my $renderer = Gtk3::CellRendererText->new;
-#add this renderer to $tree_column. This works like a Gtk3::Hbox
-# so you can add more than one renderer to $tree_column			
-$tree_column->pack_start ($renderer, 0);
-# set the cell "text" attribute to column 0   
-#- retrieve text from that column in treestore 
-# Thus, the "text" attribute's value will depend on the row's value
-# of column 0 in the model($treestore),
-# and this will be displayed by $renderer,
-# which is a text renderer
-$tree_column->add_attribute($renderer, text => 0);
+	# add renderer to $tree_column. This works like a Gtk3::Hbox
+	# so you can add more than one renderer to $tree_column			
+	$tree_column->pack_start ($renderer, 0);
 
-#add $tree_column to the treeview
-$tree_view->append_column ($tree_column);
+	# set the cell "text" attribute to column 0   
+	#- retrieve text from that column in treestore 
+	# Thus, the "text" attribute's value will depend on the row's value
+	# of column 0 in the model($treestore),
+	# and this will be displayed by $renderer,
+	# which is a text renderer
+	$tree_column->add_attribute($renderer, text => $_);
 
-# make it searchable
+	#add $tree_column to the treeview
+	$tree_view->append_column ($tree_column);
+
+	# Allow sorting on the column
+	$tree_column->set_sort_column_id($_);
+}
+
+# make searchable 
 $tree_view->set_search_column(0);
-
-# Allow sorting on the column
-$tree_column->set_sort_column_id(0);
 
 # Allow drag and drop reordering of rows
 $tree_view->set_reorderable(1);
+
+# Add treeview to scrolled window
 $sw->add($tree_view);
+
+$label = Gtk3::Label->new( '' );
+$label->set_selectable(1);
+$vbox->pack_start($label, 1, 1, 0);
 
 $window->show_all;
 Gtk3->main;
+
