08c7cc855b44676a7df3baf45f4e66e8382325a0
xxj <xxj@75a15a69-0cfc-47d9-a1bd-ce2941b697b2>
2013-10-11 22:34:54 +0000

    menu.pl - add toolbar, some syntax changes, move label on top
    git-svn-id: https://svn.code.sf.net/p/fsthost/code/trunk@263 75a15a69-0cfc-47d9-a1bd-ce2941b697b2

--- fsthost_menu.pl
+++ fsthost_menu.pl
@@ -6,12 +6,12 @@ use XML::LibXML;
 use Gtk3;
 
 use constant {
-	NOGUI => 0,
+	NOGUI => 0, # Disable GUI at all
 	GUI_HIDE => 1,
 	GUI_NORMAL => 2
 };
 
-my $FSTHOST_GUI = GUI_NORMAL;
+my $FSTHOST_GUI = GUI_NORMAL; # default
 
 my $filename = $ENV{'HOME'}."/.fsthost.xml";
 my $label;
@@ -48,10 +48,15 @@ sub tv_selection_changed {
 	$label->set_text ( get_cmd_from_tv($tv) );
 }
 
+sub edit_button_toggle {
+	my $eb = shift;
+	$FSTHOST_GUI = ( $eb->get_active() ) ? GUI_NORMAL : GUI_HIDE;
+}
+
 sub read_xml_db {
 	my $fst = shift;
 
-	my $parser = XML::LibXML->new();
+	my $parser = new XML::LibXML ();
 	my $doc    = $parser->parse_file($filename);
 	my $root = $doc->documentElement();
 	my @nodes = $root->getChildrenByTagName('fst');
@@ -73,19 +78,34 @@ read_xml_db ( \%fst );
 #print Dumper \%fst;
 
 Gtk3->init; # works if you didn't use -init on use
-my $window = Gtk3::Window->new ('toplevel');
+my $window = new Gtk3::Window ('toplevel');
 $window->signal_connect('delete_event' => sub { Gtk3->main_quit; });
 #$window->set_icon_from_file ( 'fsthost.xpm', 0 );
 #$window->set_border_width(5);
 
 # Grid
-my $grid = Gtk3::Grid->new();
+my $grid = new Gtk3::Grid;
 $grid->set_border_width( 5 );
 $window->add( $grid );
 
+# Toolbar
+my $toolbar = new Gtk3::Toolbar ();
+$grid->attach ( $toolbar, 0, 0, 1, 1 ); # left, top, width, height
+
+# Quit Button
+my $edit_button = Gtk3::ToggleToolButton->new_from_stock ( 'gtk-edit' );
+$edit_button->set_active(1);
+$edit_button->signal_connect('toggled' => \&edit_button_toggle );
+$toolbar->insert ( $edit_button, 0 );
+
+# Label for command
+$label = new Gtk3::Label ( '' );
+$label->set_selectable(1);
+$grid->attach_next_to ( $label, $toolbar, 'GTK_POS_BOTTOM', 1, 1 );
+
 # Scrolled window
 #create a scrolled window that will host the treeview
-my $sw = Gtk3::ScrolledWindow->new (undef, undef);
+my $sw = new Gtk3::ScrolledWindow (undef, undef);
 $sw->set_shadow_type ('etched-out');
 $sw->set_policy ('automatic', 'automatic');
 #This is a method of the Gtk3::Widget class,it will force a minimum 
@@ -94,10 +114,10 @@ $sw->set_policy ('automatic', 'automatic');
 $sw->set_size_request (700, 700);
 #method of Gtk3::Container
 #$sw->set_border_width(5);
-$grid->attach ( $sw, 0, 0, 1, 1 );
+$grid->attach_next_to ( $sw, $label, 'GTK_POS_BOTTOM', 1, 1 );
 
 # TreeView
-my $tree_store = Gtk3::TreeStore->new(qw/Glib::String Glib::String Glib::String/);
+my $tree_store = new Gtk3::TreeStore (qw/Glib::String Glib::String Glib::String/);
 
 #fill it with arbitry data
 foreach (sort keys %fst) { 	
@@ -107,10 +127,10 @@ foreach (sort keys %fst) {
 }
 
 #create a renderer that will be used to display info in the model
-my $renderer = Gtk3::CellRendererText->new();
+my $renderer = new Gtk3::CellRendererText();
 
 #this will create a treeview, specify $tree_store as its model
-my $tree_view = Gtk3::TreeView->new($tree_store);
+my $tree_view = new Gtk3::TreeView ($tree_store);
 
 # Connect double-click signal
 $tree_view->signal_connect('row-activated' => \&start_fsthost );
@@ -119,7 +139,7 @@ $tree_view->signal_connect('cursor-changed' => \&tv_selection_changed );
 my @columns_desc = ( 'Plugin', 'Arch' , 'Path' );
 foreach (0 .. 2) {
 	#create a Gtk3::TreeViewColumn to add to $tree_view
-	my $tree_column = Gtk3::TreeViewColumn->new();
+	my $tree_column = new Gtk3::TreeViewColumn ();
 	$tree_column->set_title ( $columns_desc[$_] );
 
 	# add renderer to $tree_column. This works like a Gtk3::Hbox
@@ -149,10 +169,6 @@ $tree_view->set_reorderable(1);
 
 # Add treeview to scrolled window
 $sw->add($tree_view);
-
-$label = Gtk3::Label->new( '' );
-$label->set_selectable(1);
-$grid->attach_next_to ( $label, $sw, 'GTK_POS_BOTTOM', 1, 1 );
 
 $window->show_all;
 Gtk3->main;
