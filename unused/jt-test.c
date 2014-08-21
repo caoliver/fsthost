04823afb0f8fee3cb14dc8fded77a851c6e647a7
xxj <xxj@75a15a69-0cfc-47d9-a1bd-ce2941b697b2>
2014-08-21 04:44:27 +0000

    Improve JT-test ( need his help to fix hydrogen ;-)
    git-svn-id: https://svn.code.sf.net/p/fsthost/code/trunk@366 75a15a69-0cfc-47d9-a1bd-ce2941b697b2

--- unused/jt-test.c
+++ unused/jt-test.c
@@ -1,27 +1,37 @@
 /*
 	Test APP for JackTransport by Xj <xj@wp.pl>
 
-	gcc -o jt-test.o jt-test.c -ljack
+	gcc -o jt-test.o jt-test.c -ljack -pthread
 */
 
 #include <stdio.h>
 #include <signal.h>
 #include <math.h>
+#include <stdbool.h>
+#include <semaphore.h>
 #include <jack/jack.h>
 
 int quit = 0;
+sem_t sem;
 
 void signal_handler(int signum) {
 	quit = 1;
+	sem_post ( &sem );
+}
+
+static int process_callback ( jack_nframes_t nframes, void* data) {
+	sem_post ( &sem );
+        return 0;
 }
 
 int main(void) {
 	jack_client_t* client;
 	jack_position_t jp;
 
-	
 	client = jack_client_open("JT TEST", JackNoStartServer, NULL);
 	if (!client) return 1;
+
+	jack_set_process_callback ( client, (JackProcessCallback) process_callback, NULL );
 	jack_activate(client);
 
         struct sigaction sa;
@@ -30,27 +40,33 @@ int main(void) {
         sa.sa_handler = &signal_handler;
         sigaction(SIGINT, &sa, NULL);
 
+	int32_t last_tick = -1;
+	int32_t last_beat = -1;
 	while (!quit) {
+		sem_wait ( &sem );
+
 		jack_transport_state_t tstate = jack_transport_query (client, &jp);
 
-		printf("Frame: %010d", jp.frame);
 		if (jp.valid & JackPositionBBT) {
-			printf(" | Bar: %04d | Beat: %d | Tick: %04d | Offset: %04d", jp.bar, jp.beat, jp.tick, jp.bbt_offset);
-
 			// Current Beat From Frame
 			double ppq = jp.frame_rate * 60 / jp.beats_per_minute;
 			double current_beat = jp.frame / ppq;
-                	printf( " | CBFF: %4.2f" , current_beat );
 
 			// Current Beat From BBT
 			double ppqBar = (jp.bar - 1) * jp.beats_per_bar;
 			double ppqBeat = jp.beat - 1;
 			double ppqTick = (double) jp.tick / jp.ticks_per_beat;
-			printf(" | CBFBBT: %4.2f", ppqBar + ppqBeat + ppqTick);
-			
+
+//			if ( last_beat != jp.beat ) {
+			if ( last_tick != jp.tick ) {
+				printf("Frame: %010d | Bar: %04d | Beat: %d | Tick: %04d | Offset: %04d | CBFF: %4.2f | CBFBBT: %4.2f\n",
+					jp.frame, jp.bar, jp.beat, jp.tick, jp.bbt_offset, current_beat, ppqBar + ppqBeat + ppqTick
+				);
+			}
+
+			last_beat = jp.beat;
+			last_tick = jp.tick;
 		}
-		putchar('\n');
-		sleep (1);
 	}
 
 	printf (".. quiting\n");
