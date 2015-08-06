cab94c1f5076b4764f4c12e0ca90cbfd27cb9c4c
xxj <xxj@75a15a69-0cfc-47d9-a1bd-ce2941b697b2>
2015-08-06 03:47:12 +0000

    Add gjfst.h
    git-svn-id: https://svn.code.sf.net/p/fsthost/code/trunk@470 75a15a69-0cfc-47d9-a1bd-ce2941b697b2

---
+++ gtk/gjfst.h
@@ -0,0 +1,12 @@
+#ifndef __gjfst_h__
+#define __gjfst_h__
+
+#include <jfst/jfst.h>
+
+void gjfst_init(int *argc, char **argv[]);
+void gjfst_add (JFST* jfst);
+void gjfst_start();
+void gjfst_quit();
+void gjfst_free(JFST* jfst);
+
+#endif /* gjfst_h */
