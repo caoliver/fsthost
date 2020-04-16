09e9d363a0bb49870e06a3777497db8086b64c8a
Christopher A. Oliver <caowasteland@gmail.com>
2020-04-16 14:44:32 -0400

    Replace spaces in parameter names with underscores for easier parsing.

--- proto.c
+++ proto.c
@@ -84,6 +84,10 @@ static void list_params_b64(JFST* jfst, ServClient* serv_client, bool brief ) {
 	for ( int ix = 0; ix < fst_num_params(fst); ix++ ) {
 	    fst_call_dispatcher ( fst, effGetParamName, ix, 0, paramName, 0 );
 	    float parm = fst->plugin->getParameter(fst->plugin, ix);
+	    for (int i = strlen(paramName); --i >= 0;)
+		if (isspace(paramName[i]))
+		    paramName[i] = '_';
+
 	    struct b64template tmpl = { parm, ix, 0, 1, 0 };
 	    
 	    encode_template(&tmpl, out);
